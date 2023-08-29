a6b10de54ec4edc3a128cdbab445aa4c8f7117c9
CamillaLucero23 <114192145+CamillaLucero23@users.noreply.github.com>
2023-08-29 09:01:37 -0600

    Homework 1 Changes

--- Homework/Homework 1.py
+++ Homework/Homework 1.py
@@ -2,10 +2,46 @@
 Homework 1
 Camilla Lucero
 8/28/2023
-Program Desc. : You're an undercover spy attempting to gain access to gain access t
+Program Desc. : 
 '''
+def compareResponseAndAnswer(response, answer):
+    #if response matches answer, return the following boolean/ 1 or 0
+    if response == answer:
+        return 1
+    else:
+        return 0
 
-#Intro Dialog
-print("Voice on Radio: Alright Agent 47, you're at the door of the vault now. Lets see what we can do to crack this puppy open... \n")
-print("Ugh, really? SECURITY QUESTIONS?? For a bank vault? I guess we gotta do this ...\nI'll give you questions, answer them on the terminal in front of you.")
 
+#initial boolean
+isAccessGranted = False
+
+#while the boolean flag is false...
+while isAccessGranted != True:
+  
+    #question 1 - math with random ints
+    #generate 2 random ints
+    from random import randint
+    randomInt1 = randint(1,150)
+    randomInt2 = randint(1,150)
+
+    print("What is the sum of the following integers? " + str(randomInt1) + " + " + str(randomInt2))
+    userResponse = input()
+
+    #check if answer is correct, if so, continue, if not, break loop
+    isCorrectAnswer = compareResponseAndAnswer(int(userResponse), randomInt1+randomInt2)
+    if isCorrectAnswer :
+
+        #question 2 - counting letters
+        countingLettersWord = 'Archipelago'
+        print("Count and input the number of letters in this word: " + countingLettersWord)
+        userResponse = input()
+        
+        #check if answer is correct. if so, continue, if not, break loop
+        isCorrectAnswer = compareResponseAndAnswer(userResponse, len(countingLettersWord))
+        if isCorrectAnswer:
+             
+    else: 
+        print("Answer incorrect. Please try again from the beginning.")
+        break
+
+        

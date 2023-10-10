ee1ae802587760cc8627d9f12b6eb95cd147f0f7
CamillaLucero23 <114192145+CamillaLucero23@users.noreply.github.com>
2023-10-10 16:44:48 -0600

    Update hw4_camilla_lucero_ex_1.py

--- Homework/hw4_camilla_lucero/hw4_camilla_lucero_ex_1.py
+++ Homework/hw4_camilla_lucero/hw4_camilla_lucero_ex_1.py
@@ -5,3 +5,30 @@ Camilla Lucero
 
 This program...
 '''
+
+#import math because... well duh
+import math
+
+#classes
+class Rectangle():
+
+    #intialize
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    #Calculate Area
+    def calculateArea(self):
+        return self.length * self.width
+    
+    #Calculate Perimeter
+    def calculatePerimeter(self):
+        return 2(self.length + self.width)
+    
+    #Calculate Diagonal
+    def calculateDiagonal(self):
+        return math.sqrt((self.length + self.width))
+    
+
+class Square(Rectangle):
+

fb8b11aaa9ce4e58c9c0fd18e7dd2804f34c908a
CamillaLucero23 <114192145+CamillaLucero23@users.noreply.github.com>
2023-10-11 09:02:16 -0600

    Update hw4_camilla_lucero_ex_2.py

--- Homework/hw4_camilla_lucero/hw4_camilla_lucero_ex_2.py
+++ Homework/hw4_camilla_lucero/hw4_camilla_lucero_ex_2.py
@@ -5,3 +5,24 @@ Camilla Lucero
 
 
 '''
+#import regex
+import re
+
+stringToTest = "(202) 225–3121 , 1–800–252–2873 , 1 (800) 843–5763 , 911"
+
+#create regex
+phoneNumRegEx = re.compile(r'''
+                           (\d)?  #funny little american number at the front(optional)
+                           (\s|-|\.)? #Seperator (Optional)
+                           (\(?\d{3}\)?)? #area code (Optional)
+                            (\s|-|\.)? #Seperator (Optional)
+                           \d{3} #3 Numbers (Required)
+                            (\s|-|\.)? #Seperator (Required)
+                           \d{4} #4 Numbers (Required)
+                           (\s*(ext|x|ext.)\s*\d{2,5})? #extension (optional)
+                            ''', re.VERBOSE)
+
+foundNumbers = phoneNumRegEx.findall(stringToTest)
+
+
+
